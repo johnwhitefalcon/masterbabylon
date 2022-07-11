0be8b4a9726853eaebebd79e5a5f406811dcaf87
johnwhitefalcon <87903601+johnwhitefalcon@users.noreply.github.com>
2022-07-11 22:07:00 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+
+var jcnode = function jcnode(){
+const express = require('express')
+const app = express();
+
+
+const j = app.use(express.static('public'))
+
+
+
+app.listen(4000);
+
+
+
+}
+
+
+module.exports = jcnode;
+
+
